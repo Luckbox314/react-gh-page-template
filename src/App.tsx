b26a4882d6c5c18f4f8db524ec263ef0037e7f16
Lucas Muñoz <50024324+Luckbox314@users.noreply.github.com>
2024-12-19 00:21:40 -0300

    feat: new text in default page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div>Template</div>;
+  return (
+    <div>
+      <h1>Luckbox's Github pages template</h1>
+      <a href="https://github.com/Luckbox314/react-gh-page-template">
+        Repository
+      </a>
+    </div>
+  );
 }
 
 export default App;
